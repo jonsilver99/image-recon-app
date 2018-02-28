ff00abe8013969b96c4c6335235cb2815a7bf6f1
John <jonatanza1@gmail.com>
2018-02-28 20:26:03 +0200

    teting api18

--- server.js
+++ server.js
@@ -6,22 +6,16 @@ let PORT = process.env.PORT || 4200;
 // this might need to be changed to use the aws sdk to serve up images from there
 let staticRegexOpen = new RegExp(/^\/open-iconic*$/);
 
-server.use((req, res, next)=>{
+server.use((req, res, next) => {
     console.log(`requested url: ${req.url}`);
     next();
 })
 
-server.get(/open-iconic/, (req, res, next)=>{
-    console.log(req.url);
-    console.log(path.join(__dirname + '/font/fonts/index.open-iconic.eot'));
-    res.sendFile(path.join(__dirname + '/font/fonts/index.open-iconic.eot'));
-    next();
-});
+server.get(/open-iconic/, express.static(__dirname + '/font/fonts'));
 
 server.use(express.static(__dirname + '/dist'));
 
 server.get('/*', (req, res, next) => {
-    console.log(req.url);
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
