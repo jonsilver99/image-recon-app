44193104cf49583b49e11a33d01a07f5f814f9ce
John <jonatanza1@gmail.com>
2018-02-28 03:48:54 +0200

    test #3

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ const path = require('path');
 let PORT = process.env.PORT || 4200;
 
 // this might need to be changed to use the aws sdk to serve up images from there
-server.use(express.static(__dirname + '/app/dist'));
+console.log('heres the dirname:');
+console.log(__dirname);
+
+server.use(express.static(__dirname + '/dist'));
 
 server.get('/*', (req, res, next) => {
-    res.sendFile(path.join(__dirname + '/app/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 })
-
-console.log(__dirname)
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
 export const environment = {
   production: true
 };
+
+export const URLS = {
+    EntryURL: "http://localhost:4400",
+    GetPicURL: `${this.EntryURL}/files/getAllPics`,
+    UploadPic: `${this.EntryURL}/files/upload`,
+    ReconPic: `${this.EntryURL}/files/recon`
+}
+
+export const EntryURL = "http://localhost:4400";
+export const RegisterURL = `${EntryURL}/register`;
+export const LoginURL = `${EntryURL}/login`;
+export const VerifyAuthURL = `${EntryURL}/api`;
+export const GetPicURL = `${EntryURL}/api/files/getAllPics`;
+export const UploadPicURL = `${EntryURL}/api/files/upload`;
+export const ReconPicURL = `${EntryURL}/api/files/recon`;
+export const LikPicURL = `${EntryURL}/api/files/like`;
+
+
