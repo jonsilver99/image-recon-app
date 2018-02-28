9f74fac1461b78a5f993eb9b3991f2dada90b5b7
John <jonatanza1@gmail.com>
2018-02-28 02:55:29 +0200

    changed path to index.html on heroku (app/dist/index.html

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 let PORT = process.env.PORT || 4200;
 
 // this might need to be changed to use the aws sdk to serve up images from there
-server.use(express.static(__dirname + '/dist'));
+server.use(express.static(__dirname + '/app/dist'));
 
 server.get('/*', (req, res, next) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/app/dist/index.html'));
 });
 
 server.listen(PORT, () => {
